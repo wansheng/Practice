da9edac76e4696af05359af41020fd4c9c00ae88
wansheng <wansheng@gmail.com>
2014-04-22 16:18:46 +0800

    check in todo.js

---
+++ Practice/03FireBase/Todo.js
@@ -0,0 +1,42 @@
+﻿function todoController($scope) {
+    $scope.fb = new Firebase('https://sweltering-fire-4693.firebaseio.com/todos/');
+    $scope.todos = [];
+    $scope.initialed = false;
+ 
+    $scope.fb.on('value', fbValueChanged);
+    function fbValueChanged(snapeshot) {
+        $scope.todos = snapeshot.val();
+        if (!$scope.$$phase) {
+            $scope.$apply();
+        }
+        $scope.initialed = true;
+    }
+    $scope.addToDo = function () {
+        $scope.fb.push({ desc: $scope.todoText, done: false, dueDate: $scope.todoDueDate });
+        $scope.todoText = '';
+    };
+    $scope.$watch(function () {
+        var result = 0;
+        angular.forEach($scope.todos, function (todo) {
+            result += todo.done ? 1 : 0;
+        });
+        return result;
+    }, function (newValue, oldValue) {
+        if ($scope.initialed == true)
+        {
+            $scope.fb.set($scope.todos);
+        }
+    });
+    $scope.remaining = function () {
+        var count = 0;
+        angular.forEach($scope.todos, function (todo) {
+            count += todo.done ? 0 : 1;
+        });
+        return count;
+    };
+    $scope.pluralizer = {
+        0: "Finished all",
+        1: "Only one left! GJ",
+        other:"{} items remain in your todo list."
+    }
+}
