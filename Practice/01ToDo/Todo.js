2493c300cd5247177d4bc7e145cf573a9123b76f
wansheng <wansheng@gmail.com>
2014-05-09 14:21:18 +0800

    Add remove method, change public method to Pascal

--- Practice/01ToDo/Todo.js
+++ Practice/01ToDo/Todo.js
@@ -1,13 +1,25 @@
 ﻿function todoController($scope) {
-    $scope.todos = [];
-    $scope.addToDo = function () {
-        $scope.todos.push({ desc: $scope.todoText, done: false,dueDate:$scope.todoDueDate });
-        $scope.todoText = '';
+    $scope.Todos = [];
+    $scope.TodoDueDate = now();
+
+    $scope.AddToDo = function () {
+        $scope.Todos.push({ Desc: $scope.TodoText, Done: false,DueDate:$scope.TodoDueDate });
+        $scope.TodoText = '';
     };
-    $scope.remaining = function () {
+    $scope.RemoveCompleted = function () {
+        for (var i = 0; i < $scope.Todos.length ; i++)
+        {
+            if ($scope.Todos[i].Done)
+            {
+                $scope.Todos.splice(i, 1);
+                i--;
+            }
+        }
+    };
+    $scope.Remaining = function () {
         var count = 0;
-        angular.forEach($scope.todos, function (todo) {
-            count += todo.done ? 0 : 1;
+        angular.forEach($scope.Todos, function (todo) {
+            count += todo.Done ? 0 : 1;
         });
         return count;
     };
@@ -16,4 +28,20 @@
         1: "Only one left! GJ",
         other:"{} items remain in your todo list."
     }
+    function now() {
+        var d = new Date();
+        return d.getFullYear() + '-' + format2Digits(d.getMonth() + 1) + '-' + format2Digits(d.getDate());
+       
+
+    }
+    function format2Digits(int2Format)
+    {
+        var string2Format = int2Format.toString();
+        if (string2Format.length == 1)
+        {
+            return "0" + string2Format;
+        }
+        return string2Format
+    }
+
 }
