fddb1bd149cecc3f0cabfe9ddf60ebd5084806a8
wansheng <wansheng@gmail.com>
2014-05-09 17:10:51 +0800

    Remove Todo Function.

--- Practice/03HttpModule/Todo.js
+++ Practice/03HttpModule/Todo.js
@@ -2,13 +2,13 @@
 
 ngTodo.factory("TodoOperator", function ($http) {
     return {
-        'getTodo': function ($scope) {
+        'GetTodo': function ($scope) {
             $http({ method: 'GET', url: 'https://sweltering-fire-4693.firebaseio.com/todos/.json' })
             .success(function (data, status, headers, config) {
                 // this callback will be called asynchronously
                 // when the response is available
-                //var a = data;
                 todos = [];
+                if (data != "null")
                 for (var i in data) {
                     todos.push(data[i]);
                 }
@@ -19,45 +19,68 @@ ngTodo.factory("TodoOperator", function ($http) {
                 alert(data);
             })
         },
-        'postTodo': function ($scope) {
-            $http({ method: 'POST', url: 'https://sweltering-fire-4693.firebaseio.com/todos/.json', data: $scope.newTodo })
-      .success(function (data, status, headers, config) {
-          alert('success');
-          $scope.Todos.push($scope.newTodo);
-          $scope.todoText = '';
-      })
-      .error(function (data, status, headers, config) {
-          alert('error');
-      })
+        'AddTodo': function ($scope, newTodo) {
+
+            $http({ method: 'PUT', url: 'https://sweltering-fire-4693.firebaseio.com/todos/' + newTodo.ID + '/.json', data: newTodo })
+            .success(function (data, status, headers, config) {
+                $scope.Todos.push(newTodo);
+                $scope.TodoText = '';
+            })
+            .error(function (data, status, headers, config) {
+                alert('error');
+            })
+        },
+        'RemoveTodo': function ($scope, deleteTodo) {
+            $http({ method: 'DELETE', url: 'https://sweltering-fire-4693.firebaseio.com/todos/' + deleteTodo.ID + '.json', data: deleteTodo })
+     .success(function (data, status, headers, config) {
+         for (var i = 0; i < $scope.Todos.length ; i++) {
+             if ($scope.Todos[i].Done && $scope.Todos[i].Desc == deleteTodo.Desc && $scope.Todos[i].DueDate == deleteTodo.DueDate) {
+                 $scope.Todos.splice(i, 1);
+                 return;
+             }
+         }
+     })
+     .error(function (data, status, headers, config) {
+         alert('error');
+     })
         }
     }
-})
+});
 
 ngTodo.controller("todoController", function ($scope, TodoOperator) {
 
     $scope.Todos = [];
-    $scope.Initialed = false;
+    $scope.TodoDueDate = now();
     $scope.DebugResponse = '';
     initial();
 
-    function initial() {
-        TodoOperator.getTodo($scope);
-    }
-    $scope.addToDo = function () {
-        $scope.newTodo = { desc: $scope.todoText, done: false, dueDate: $scope.todoDueDate };
-        TodoOperator.postTodo($scope);
+    $scope.AddToDo = function () {
+        var id = getGuid();
+        var newTodo = { Desc: $scope.TodoText, Done: false, DueDate: $scope.TodoDueDate, ID: id };
+        TodoOperator.AddTodo($scope, newTodo);
     };
-
-    $scope.remaining = function () {
+    $scope.RemoveCompleted = function () {
+        for (var i = 0; i < $scope.Todos.length ; i++) {
+            if ($scope.Todos[i].Done) {
+                TodoOperator.RemoveTodo($scope, $scope.Todos[i]);
+            }
+        }
+    };
+    $scope.Remaining = function () {
         var count = 0;
         angular.forEach($scope.Todos, function (todo) {
             count += todo.done ? 0 : 1;
         });
         return count;
     };
-    $scope.pluralizer = {
+    $scope.Pluralizer = {
         0: "Finished all",
         1: "Only one left! GJ",
         other: "{} items remain in your todo list."
+    }
+
+
+    function initial() {
+        TodoOperator.GetTodo($scope);
     }
 });
